6ddbfab12eef4fc52926d28f1a9c1330a6e83ebc
5PK <1trankev@gmail.com>
2018-05-15 16:22:15 +0100

    completed part 5 of Django Polls app

--- TestProjects/polls/views.py
+++ TestProjects/polls/views.py
@@ -5,6 +5,7 @@ from django.shortcuts import get_object_or_404, render
 from django.urls import reverse
 from .models import Question, Choice
 from django.views import generic
+from django.utils import timezone
 
 # OLD VIEWS - REPITITIVE FUNCTIONALITY
 
@@ -47,8 +48,16 @@ class IndexView(generic.ListView):
     context_object_name = 'latest_question_list'
 
     def get_queryset(self):
-        """Return the last five published questions."""
-        return Question.objects.order_by('-pub_date')[:5]
+        #"""Return the last five published questions."""
+        #return Question.objects.order_by('-pub_date')[:5]
+
+        """
+        Return the last five published questions (not including those set to be
+        published in the future).
+        """
+        return Question.objects.filter(
+            pub_date__lte=timezone.now()
+        ).order_by('-pub_date')[:5]
 
 
 class DetailView(generic.DetailView):
